44c111172c2f7e0b057151c7c53dfac0fc27b229
Vraiment <jemc44@gmail.com>
2018-05-24 23:28:41 -0700

    Added relevant properties and documentation to the JniPluginExtension class

--- src/main/groovy/com/vraiment/gradle/jni/JniPluginExtension.groovy
+++ src/main/groovy/com/vraiment/gradle/jni/JniPluginExtension.groovy
@@ -1,14 +1,61 @@
 package com.vraiment.gradle.jni
 
 import org.gradle.api.Project
+import org.gradle.api.file.FileCollection
 
+/**
+ * Extension which contains general values for the plugin.
+ */
 class JniPluginExtension {
+    /**
+     * The list of classes that contain native methods.
+     */
     List<String> classes
-    String headersDir
 
+    /**
+     * The output directory that will contain the generated sources dir.
+     */
+    File generatedHeadersDir
+
+    /**
+     * The input directory that contains the Makefile for the native implementation.
+     */
+    File makeSourcesDir
+
+    /**
+     * The output directory for native artifacts.
+     */
+    File makeOutputDir
+
+    /**
+     * The directory that contains the $JVM_HOME value.
+     */
+    File jvmHome
+
+    /**
+     * The classpath that will be used for generating the header files and executing the Makefile.
+     */
+    FileCollection classPath
+
+    /**
+     * Initialize this extension object with the given project.
+     */
     JniPluginExtension(Project project) {
-        // These should initialize to useful values
         classes = []
-        headersDir = ''
+        generatedHeadersDir = new File("${project.projectDir}/src/jni")
+        makeSourcesDir = generatedHeadersDir
+        makeOutputDir = new File("${project.buildDir}/jni")
+        jvmHome = extractJvmHome(project)
+        classPath = extractClassPath(project)
+    }
+
+    private String extractJvmHome(project) {
+        // TODO
+        return null
+    }
+
+    private String extractClassPath(project) {
+        // TODO
+        return null
     }
 }
