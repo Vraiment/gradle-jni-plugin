23a657c636a68584676584880dae7fd04a6dfdb8
Vraiment <jemc44@gmail.com>
2018-05-28 16:10:12 -0700

    Updated and added documentation to the AbstractMakeTask

--- src/main/groovy/com/vraiment/gradle/jni/task/AbstractMakeTask.groovy
+++ src/main/groovy/com/vraiment/gradle/jni/task/AbstractMakeTask.groovy
@@ -1,25 +1,48 @@
 package com.vraiment.gradle.jni.task
 
+import org.gradle.api.logging.Logger
+import org.gradle.api.logging.Logging
 import org.gradle.api.tasks.AbstractExecTask
 
+import static com.vraiment.gradle.jni.Util.validateAndCreateDir
+
+/**
+ * Abstract class to execute a Makefile.
+ */
 class AbstractMakeTask extends AbstractExecTask {
-    String sourcesDir
+    private static final Logger logger = Logging.getLogger(AbstractMakeTask)
 
-    String outputDir
+    /**
+     * The directory that contains the Makefile.
+     */
+    File makeFileDir
 
+    /**
+     * The path where the Makefile should output the build artifacts.
+     */
+    File outputDir
+
+    /**
+     * Constructs a new instance of the AbstractMakeTask class.
+     */
     AbstractMakeTask(Class taskType) {
         super(taskType)
 
         executable = 'make'
     }
 
+    /**
+     * Executes the makefile in "makeFileDir" with the given arguments.
+     */
     protected void execMake(String... arguments) {
-        assert sourcesDir != null
-        assert outputDir != null
+        assert makeFileDir?.directory : 'makeFileDir should point to a directory'
+        validateAndCreateDir(outputDir, 'outputDir')
 
-        environment 'OUTPUT', outputDir.replace(' ', '\\ ')
+        environment 'OUTPUT', outputDir.path
 
-        args = [ '-C', sourcesDir ] + arguments.toList()
+        args = [ '-C', makeFileDir ] + arguments.toList()
+
+        logger.info("Executing Makefile in $makeFileDir with output as $outputDir")
 
         super.exec()
     }
