5b3dd6b32f574ec82d8560d080df2c7ddc435421
Vraiment <jemc44@gmail.com>
2018-05-28 16:40:03 -0700

    Added documentation and general improvements to Make*Task classes

--- src/main/groovy/com/vraiment/gradle/jni/task/MakeCleanJniTask.groovy
+++ src/main/groovy/com/vraiment/gradle/jni/task/MakeCleanJniTask.groovy
@@ -1,14 +1,26 @@
 package com.vraiment.gradle.jni.task
 
+import org.gradle.api.logging.Logger
+import org.gradle.api.logging.Logging
 import org.gradle.api.tasks.TaskAction
 
+/**
+ * Task to execute "clean" on a Makefile.
+ */
 class MakeCleanJniTask extends AbstractMakeTask {
+    private static final Logger logger = Logging.getLogger(MakeCleanJniTask)
+
+    /**
+     * Constructs a new instance of MakeCleanJniTask.
+     */
     MakeCleanJniTask() {
         super(MakeCleanJniTask)
     }
 
     @TaskAction
     protected void exec() {
+        logger.info("Executing make clean")
+
         super.execMake('clean')
     }
 }
--- src/main/groovy/com/vraiment/gradle/jni/task/MakeJniTask.groovy
+++ src/main/groovy/com/vraiment/gradle/jni/task/MakeJniTask.groovy
@@ -1,19 +1,34 @@
 package com.vraiment.gradle.jni.task
 
+import org.gradle.api.logging.Logger
+import org.gradle.api.logging.Logging
 import org.gradle.api.tasks.TaskAction
 
+/**
+ * Task to execute a Makefile with no arguments but setting the JVM home.
+ */
 class MakeJniTask extends AbstractMakeTask {
-    String jvmHome
+    private static final Logger logger = Logging.getLogger(MakeJniTask)
 
+    /**
+     * The directory that contains the JVM home.
+     */
+    File jvmHome
+
+    /**
+     * Constructs a new instance of MakeJniTask.
+     */
     MakeJniTask() {
         super(MakeJniTask)
     }
 
     @TaskAction
     protected void exec() {
-        assert jvmHome != null
+        assert jvmHome?.directory : 'jvmHome should point to a directory'
 
-        environment 'JVM_HOME', jvmHome
+        environment 'JVM_HOME', jvmHome.path
+
+        logger.info("Executing makefile with JVM home in ${jvmHome.path}")
 
         super.execMake()
     }
